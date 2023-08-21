fe2b2baaf94ce27b5a373a2cd031b4ef0312a3a4
Fereshteh Rahbari <fereshteh.rahbari@gmail.com>
2023-08-21 18:05:00 +1000

    Build the ColorMode Switch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,14 @@
-import { HStack, Image, Text } from "@chakra-ui/react"
-import logo from "../assets/Logo.webp"
-
+import { HStack, Image } from "@chakra-ui/react";
+import logo from "../assets/Logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-
-    <HStack>
-      <Image src={logo} boxSize='150px'></Image>
-      <Text>Welcome to FreshMazzeh</Text>
+    <HStack justifyContent="space-between" padding="10px">
+      <Image src={logo} boxSize="150px"></Image>
+      <ColorModeSwitch></ColorModeSwitch>
     </HStack>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;

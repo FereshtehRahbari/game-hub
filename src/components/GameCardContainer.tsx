cdaf51354b653e71aecdcc24f874debb544a8ee0
Fereshteh Rahbari <fereshteh.rahbari@gmail.com>
2023-08-31 19:18:16 +1000

    Refactor: Remove Duplicated Styles

---
+++ src/components/GameCardContainer.tsx
@@ -0,0 +1,13 @@
+import { Box } from '@chakra-ui/react'
+import { ReactNode } from 'react'
+
+interface Props{
+  children:ReactNode
+}
+const GameCardContainer = ({children}: Props) => {
+  return (
+    <Box  width="360px" borderRadius="10px" overflow={"hidden"}>{children}</Box>
+  )
+}
+
+export default GameCardContainer
--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -3,8 +3,8 @@ import { Card, CardBody, Skeleton, SkeletonText } from '@chakra-ui/react'
 
 const GameCardSkeleton = () => {
   return (
-    <Card width="300px" borderRadius="10px" overflow={"hidden"}>
-      <Skeleton height="220px"/>
+    <Card>
+      <Skeleton height="200px"/>
       <CardBody>
         <SkeletonText/>
       </CardBody>
